b8aee5c32dca8a30c73e13f6f7854c36aaa98825
Fayeeza Subah Islam <fayeezaislam1418@gmail.com>
2023-09-28 22:28:35 +1000

    Login and Logout with role based registration created using Django built in user model

--- bright_boost/brightBoostApp/models.py
+++ bright_boost/brightBoostApp/models.py
@@ -18,6 +18,3 @@ class TutorSchedule(models.Model):
     day_of_week = models.CharField(max_length=20)
     start_time = models.TimeField()
     end_time = models.TimeField()
-
-
-
--- bright_boost/brightBoostApp/urls.py
+++ bright_boost/brightBoostApp/urls.py
@@ -1,18 +1,20 @@
-from django.conf import settings
 from django.urls import path
 from . import views
-from django.conf.urls.static import static
 
-# urlpatterns = [
-#     path('brightBoostApp/', views.brightBoostApp, name='brightBoostApp'),
-# ]
+from django.urls import path
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path ('home/', views.home,  name='home'),
+    path('',views.home,name='home'),
+    path('register/', views.registerView, name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('timetable/', views.display_timetable, name='timetable'),
+    # path('student_dashboard/', views.student_dashboard, name='student_dashboard'),
+    path('dashboard/', views.dashboardView, name='dashboard'),
+    path('logout/', LogoutView.as_view(next_page='home'), name="logout"),
     path('add_session/', views.add_session, name='add_session'),
-    path('timetable/', views.display_timetable, name='display_timetable'),
-     path('sessions_list/', views.sessions_list, name='sessions_list'),
-   
-] 
-static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
+    path('sessions_list/', views.sessions_list, name='sessions_list')
+  
+    # Define other URL patterns here
+]
